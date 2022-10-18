505527922d799695989e7e1a2270999df8944b92
Andreloren <andre.lorente83@gmail.com>
2022-10-18 17:08:01 -0300

    Manipulando DOM

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(tipo) {
+  const colors = {
+    visa: ["#007A87", "#9EB21F"],
+    mastercard: ["#C64F47", "#DF6F29"],
+    elo: ["#2B29B1", "#79180B"],
+    default: ["black", "gray"],
+  }
+  ccBgColor01.setAttribute("fill", colors[tipo][0])
+  ccBgColor02.setAttribute("fill", colors[tipo][1])
+  ccLogo.setAttribute("src", `cc-${tipo}.svg`)
+}
+globalThis.setCardType = setCardType
+setCardType("elo")
